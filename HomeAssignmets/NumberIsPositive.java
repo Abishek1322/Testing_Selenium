78105ba0ddebaa1bb85f0143ff6db43f16fba0f0
Abishek Kumar P <100253670+Abishek1322@users.noreply.github.com>
2025-04-05 14:26:37 +0530

    Add files via upload

---
+++ HomeAssignmets/NumberIsPositive.java
@@ -0,0 +1,20 @@
+package HomeAssignments;
+
+public class NumberIsPositive {
+
+	public static void main(String[] args) {
+		int n = 10;
+		
+		if (n<0) { //condition checks if the given number is less than zero for negative
+			System.out.println("Number is Negative");
+		}else if (n>0) { //condition checks if the given number is less than zero for Positive
+			System.out.println("Number is Positive");
+				
+			}
+		else { //condition checks if the given number is equal to zero
+			System.out.println("Number is zero");
+		}
+
+	}
+
+}
