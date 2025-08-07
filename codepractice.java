50f0e64765e6011cdd1a0e6c8b84bdac1688a2c9
Abishek1322 <abishekkumar@outlook.com>
2025-08-07 22:54:44 +0530

    Basic Program of Java

---
+++ codepractice.java
@@ -0,0 +1,8 @@
+public class codepractice{
+    public static void main(String[] args) {
+        System.out.println("Selenium code practice started.");
+        // This is a placeholder for the main method.
+        // You can add your Selenium code here to perform tests.
+        codepractice practice = new codepractice();
+    }
+}
